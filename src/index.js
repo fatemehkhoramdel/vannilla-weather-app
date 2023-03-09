7c7345c86bfcfa441502031b216f1da7eba750ce
fatemehkhoramdel <f70khd@gmail.com>
2023-03-09 18:46:33 +0330

    showing current temp

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let descriptionElement = document.querySelector("#description");
   let cityElement = document.querySelector("#city");
-  let humidityElement = document.querySelector("#humidity");
+  let humidityElement = document.querySelector("#Humidity");
+  let windElement = document.querySelector("#Wind");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
-
   cityElement.innerHTML = response.data.name;
-
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 axios.get(apiUrl).then(displayTemperature);
